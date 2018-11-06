0a6338069ecd9d627fba659f56b20f6d09bd71d4
Elliot Nunn <elliotnunn@me.com>
2018-11-06 12:28:18 +0800

    tick, looking very good!

--- all.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./FillGaps -o /tmp/ms && MakeHFS -s 256M -n MacStoopid -i /tmp/ms -a 'BootableMPW:MPW Shell' --mpw-dates /tmp/ms.dmg && ~/empw/M*.app/Contents/MacOS/* /tmp/ms.dmg && DumpHFS /tmp/ms.dmg .
